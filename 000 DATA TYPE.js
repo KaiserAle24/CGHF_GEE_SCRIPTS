4776c669e899c259428963958a9cba4e741c2d1c
Alex_Amaya_UNAM <121212252+KaiserAle24@users.noreply.github.com>
2023-01-11 16:12:05 -0600

    Add files via upload

---
+++ 000 DATA TYPE.js
@@ -0,0 +1,44 @@
+/* TIPOS DE DATOS
+
+Boolean	/	True o False
+
+Decimal / 16 bytes / 
+
+De 0 a +/-79 228 162 514 264 337 593 543 950 335 (+/-7,9...E+28) † sin separador decimal; 
+de 0 a +/-7,9228162514264337593543950335 con 28 posiciones a la derecha del separador decimal;
+el número menor distinto de cero es +/-0,0000000000000000000000000001 (+/-1E-28) †.
+
+Byte / De 0 a 255 (256 posibilidades)
+
+Single (punto flotante de precisión sencilla)/ 4 bytes
+
+De -3,4028235E+38 a -1,401298E-45 † para valores negativos;
+de 1,401298E-45 a 3,4028235E+38 † para valores positivos.
+
+Double (punto flotante de precisión doble)/	8 bytes
+
+De -1,79769313486231570E+308 a -4,94065645841246544E-324 † para valores negativos;
+de 4,94065645841246544E-324 a 1,79769313486231570E+308 † para valores positivos.
+
+Short (entero corto)	2 bytes	
+De -32 768 a 32 767 (con signo).
+
+Entero 4 bytes	
+De -2 147 483 648 a 2 147 483 647 (con signo).
+
+Long (entero largo)	8 bytes	
+De -9 223 372 036 854 775 808 a 9 223 372 036 854 775 807 (9,2...E+18 †) (con signo).
+
+† En notación científica:
+"E" se refiere a una potencia de 10. 
+Por lo tanto, 3,56E+2 significa 3,56 × 102 o 356, y 3,56E-2 significa 3,56 / 102 o 0,0356.
+
+String (longitud variable)/
+Depende de la plataforma de implementación.	
+De 0 a 2000 millones de caracteres Unicode aproximadamente.
+
+DateTime /	8 bytes	
+De 00:00:00 (medianoche) del 1 de enero de 0001 a 11:59:59 del 31 de diciembre de 9999.
+
+
+*/
